f419cf5e9eed4feec3aa8464ae21dab80180ced5
Jain <vikas.e.jain@accenture.com>
2017-06-06 10:48:43 +0530

    filereading

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ language = [{'name':'JS'},{'name':'python'}]
 def webhook():
     req = request.get_json(silent=True, force=True)
     filepath = req.get("filename")
-	#if filepath != filename:
-	 # filename= filepath
+    if filepath != filename:
+      filename= filepath
 	 
 	 #fileread(filename)
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
@@ -19,33 +19,15 @@ def webhook():
     dataset = pd.read_csv(url, names=names) 
     data = dataset
     l1 = list(data)	
-    return jsonify({'list':filepath})
+    return jsonify({'list':filename})
 
 
-@app.route('/sign_s3/')
-def sign_s3():
-  S3_BUCKET = os.environ.get('S3_BUCKET')
-
-  file_name = request.args.get('file_name')
-  file_type = request.args.get('file_type')
-
-  s3 = boto3.client('s3')
-
-  presigned_post = s3.generate_presigned_post(
-    Bucket = S3_BUCKET,
-    Key = file_name,
-    Fields = {"acl": "public-read", "Content-Type": file_type},
-    Conditions = [
-      {"acl": "public-read"},
-      {"Content-Type": file_type}
-    ],
-    ExpiresIn = 3600
-  )
-
-  return json.dumps({
-    'data': presigned_post,
-    'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
-  })
+@app.route('/predict', method = ['POST'])
+def predictVariable():
+  req = request.get_json(silent=True, force=True)
+  
+  return json
+ 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port = port)
