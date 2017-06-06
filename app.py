130e1672ab9ae3f4196d40c5587f22db2ac45515
Jain <vikas.e.jain@accenture.com>
2017-06-06 12:30:36 +0530

    filereading

--- app.py
+++ app.py
@@ -14,8 +14,6 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
-data= pd.DataFrame()
-
 app = Flask(__name__)
 language = [{'name':'JS'},{'name':'python'}]
 @app.route('/webhook', methods=['POST'])
@@ -26,16 +24,19 @@ def webhook():
     model = req.get("model")
     names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
     dataset = pd.read_csv(filepath, names=names) 
-    z= data.combine(dataset)
+    data = dataset
     array = data.values
     X = array[:,0:]
     Y = array[:,-1]
     validation_size = 0.20
     seed = 7
     X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
-    
-    return jsonify({'coulumn': list(z) })
+    model = modelselection(model)	
+    return jsonify({'coulumn': str(model)})
 
+
+def modelselection(model):
+    return{'LR': LogisticRegression(),'LDA': LinearDiscriminantAnalysis(),'KNN': KNeighborsClassifier(),'CART': DecisionTreeClassifier(),'NB': GaussianNB(),'SVM', SVC()}[model]
  
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
