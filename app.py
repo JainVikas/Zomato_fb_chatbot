42904a3cb8e6f2ad897ef9275cd68cff0bce850d
Jain <vikas.e.jain@accenture.com>
2017-06-07 14:32:17 +0530

    s3 bucket impementation

--- app.py
+++ app.py
@@ -62,15 +62,19 @@ def webhook():
     return jsonify({'score':score})
 #AWS s3 bucket for file uploads to be read later by webhook
 
-@app.route('/sign_s3/')
+@app.route('/sign-s3/')
 def sign_s3():
+  # Load necessary information into the application
   S3_BUCKET = os.environ.get('S3_BUCKET')
 
-  file_name = request.args.get('file_name')
-  file_type = request.args.get('file_type')
+  # Load required data from the request
+  file_name = request.args.get('file-name')
+  file_type = request.args.get('file-type')
 
+  # Initialise the S3 client
   s3 = boto3.client('s3')
 
+  # Generate and return the presigned URL
   presigned_post = s3.generate_presigned_post(
     Bucket = S3_BUCKET,
     Key = file_name,
@@ -82,6 +86,7 @@ def sign_s3():
     ExpiresIn = 3600
   )
 
+  # Return the data to the client
   return json.dumps({
     'data': presigned_post,
     'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
