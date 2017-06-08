19e079c0a4fe6f5cd2780a64e11ec825f35feb86
Jain <vikas.e.jain@accenture.com>
2017-06-08 15:31:56 +0530

    aws upload

--- app.py
+++ app.py
@@ -72,13 +72,8 @@ def upload():
     if request.method == 'POST':
         file = request.files['file']
         filename = secure_filename(file.filename)
-        #s3 = boto3.resource('s3', aws_access_key_id= os.environ.get('AWS_ACCESS_KEY_ID'), aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),config=Config(signature_version='s3v4'))
-        s3Conn = boto.connect_s3(os.environ.get('AWS_ACCESS_KEY_ID'),os.environ.get('AWS_SECRET_ACCESS_KEY'))
-        bucket2 = self.s3Conn.lookup(os.environ.get('S3_BUCKET'))
-        k = Key(bucket2)
-        k.Key = filename
-        k.set_contents_from_filename(filename)
-        #s3.Bucket(os.environ.get('S3_BUCKET')).upload_file(filename,filename)
+        s3 = boto3.resource('s3', aws_access_key_id= os.environ.get('AWS_ACCESS_KEY_ID'), aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),config=Config(signature_version='s3v4'))
+        s3.Bucket(os.environ.get('S3_BUCKET')).upload_file(filename,filename)
         return jsonify({'successful upload':filename, 'S3_BUCKET':os.environ.get('S3_BUCKET'), 'ke':os.environ.get('AWS_ACCESS_KEY_ID'), 'sec':os.environ.get('AWS_SECRET_ACCESS_KEY'),'filepath': "https://s3.us-east-2.amazonaws.com/"+os.environ.get('S3_BUCKET')+"/" +filename})
     return jsonify({'score':'correct'})
       
