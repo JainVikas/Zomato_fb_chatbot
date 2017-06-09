84258689da3baa413d84f8f1b35b86e72503cfd6
Jain <vikas.e.jain@accenture.com>
2017-06-09 10:25:37 +0530

    aws upload

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def upload():
     if request.method == 'POST':
         file = request.files['file']
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['app/.heroku/templates'], filename))
+        file.save(os.path.join(app.config['templates'], filename))
         s3 = boto3.resource('s3', aws_access_key_id= os.environ.get('AWS_ACCESS_KEY_ID'), aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),config=Config(signature_version='s3v4'))
         '''credentials = { 
             'aws_access_key_id': os.environ.get('AWS_ACCESS_KEY_ID'),
@@ -89,7 +89,7 @@ def upload():
         #s3.Bucket(os.environ.get('S3_BUCKET')).put_object(Key=filename, Body=open(file, 'rb'), ContentEncoding='text/csv')
         #s3.Object(os.environ.get('S3_BUCKET'), filename).put(Body=open(filename, 'rb'))
         #s3.Bucket(os.environ.get('S3_BUCKET')).upload_file(filename,filename)
-        with open(os.path.join(app.config['UPLOAD_FOLDER'], filename), 'rb') as f:
+        with open(os.path.join(app.config['templates'], filename), 'rb') as f:
             s3.Bucket(os.environ.get('S3_BUCKET')).put_object(Key=filename, Body=f, ContentEncoding='text/csv')
         #s3.Bucket('bucket').put_object(Key=filename, Body=file.stream, ContentEncoding='text/csv')
         return jsonify({'successful upload':filename, 'S3_BUCKET':os.environ.get('S3_BUCKET'), 'ke':os.environ.get('AWS_ACCESS_KEY_ID'), 'sec':os.environ.get('AWS_SECRET_ACCESS_KEY'),'filepath': "https://s3.us-east-2.amazonaws.com/"+os.environ.get('S3_BUCKET')+"/" +filename})
