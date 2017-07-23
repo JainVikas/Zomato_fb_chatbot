6c7b4a3066ad5925213e91d1c5253ea56f0f3ae6
Jain <vikas.e.jain@accenture.com>
2017-07-23 18:11:08 +0530

    testing

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def restaurant():
         review_dict["image_url"] = ""
         
         #restaurant_dict["buttons"] = button 
-        output["messages"][0]["attachment"]["payload"]["elements"].append(review_dict)
+        output["messages"]["attachment"]["payload"]["elements"].append(review_dict)
     output["redirect_to_blocks"] = ["call"]
     #output["messages"][0]["attachment"]["payload"]["buttons"]=[{"type":"phone_number", "phone_number": "+919538668889","title": "Call to reserve"},{"type":"show_block", "block_name":"restaurant","title":"Try another"}]
     print(output)
